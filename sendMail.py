273862a5293066816852164c50303f593014a1c1
Niv Ben Abat <niv.benavat@gmail.com>
2019-01-02 17:34:31 +0200

    Update sendMail.py

--- sendMail.py
+++ sendMail.py
@@ -1,12 +1,21 @@
 import smtplib
 
-fadd = ' # senders email address'
-tadd = '# receivers email address'
+fadd = ' # senders Email address'
+tadd = '# receivers Email address'
 msg = 'Mail sent through Python!'
-username = '# Your username(email ID)'
+username = '# Your username(Email ID)'
 password = '# Your password for above Email ID'
 server = smtplib.SMTP('SMTP.GMAIL.COM', 587)
 server.ehlo()
 server.starttls()
 server.login(username, password)
 server.sendmail(fadd, tadd, msg)
+
+
+
+
+# So Simple :)
+
+
+
+
